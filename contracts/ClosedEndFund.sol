7273077260bfd5d948d37a7da4d780c515d2ee54
Tunc Polat <tunc.polat@simplweb.ch>
2022-04-12 13:21:01 +0200

    Init erc20 token

--- contracts/ClosedEndFund.sol
+++ contracts/ClosedEndFund.sol
@@ -2,14 +2,26 @@
 
 pragma solidity ^0.8.12;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract CEFToken is ERC20 {
+    constructor(uint256 initialSupply) payable ERC20("CEF TOKEN", "CEF") {
+        _mint(msg.sender, initialSupply);
+    }
+}
+
 contract ClosedEndFund {
     address public manager; // address of the fund manager
-    string public name; // name of the fund
+    string public title; // name of the fund
     string public description; // description of the fund
 
-    constructor(string memory _name, string memory _description) {
+    constructor(
+        string memory _title,
+        string memory _description,
+        uint256 _initialSupply
+    ) CEFToken(_initialSupply) {
         manager = msg.sender;
-        name = _name;
+        title = _title;
         description = _description;
     }
 }
