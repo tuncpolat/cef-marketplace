c2d701f5da2c71ce3a51dcfbbdef21c7339f8e11
Tunc Polat <tunc.polat@simplweb.ch>
2022-04-20 10:37:46 +0200

    Add new token time frame in waiting list

--- client/src/pages/funds/wl.js
+++ client/src/pages/funds/wl.js
@@ -263,6 +263,30 @@ export default function WaitingListPage() {
               htmlFor="first-name"
               className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
             >
+              Set new timeframe to buy
+            </label>
+            <div className="flex mt-4 sm:mt-0 sm:col-span-2">
+              <input
+                placeholder="New cap in WEI"
+                type="text"
+                name="first-name"
+                id="first-name"
+                autoComplete="given-name"
+                className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
+              />
+              <button
+                type="submit"
+                className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Set new timeframe
+              </button>
+            </div>
+          </div>
+          <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+            <label
+              htmlFor="first-name"
+              className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+            >
               Add investor to whitelist
             </label>
             <div className="flex mt-4 sm:mt-0 sm:col-span-2">
